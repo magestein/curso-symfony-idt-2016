792bc83d4c2f1a67e7e891a4d0d997deb05dfd28
Juan Ardissone <juan.ardissone@asuncion.gov.py>
2016-08-09 19:40:43 -0400

    Primer commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+curso-idt
+=========
+
+A Symfony project created on July 28, 2016, 9:23 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $a = '<strong>holaaaaa</strong>';
+        $foo = uniqid();
+
+        return $this->render('default/index.html.twig', array(
+            'var1' => $foo,
+            'a' => $a
+        ));
+    }
+
+}
