09faa95de24f89a00175311f6ba9009e4d1bee55
Vicente Insfrán C <vinsfran@gmail.com>
2016-09-22 18:48:38 -0400

    restauracion de  nombre Variable $a en DefaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,12 +11,12 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $a1 = '<strong>holaaaaa</strong>';
+        $a = '<strong>holaaaaa</strong>';
         $foo = uniqid();
 
         return $this->render('@App/default/index.html.twig', array(
             'var1' => $foo,
-            'a' => $a1
+            'a' => $a
         ));
     }
 
