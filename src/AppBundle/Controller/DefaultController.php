9e154924a76fbb6620efc30a4e2a385e07319a67
Vicente Insfrán C <vinsfran@gmail.com>
2016-09-22 18:55:08 -0400

    cambio de  nombre Variable $a a $a2 en DefaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,12 +11,12 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $a = '<strong>holaaaaa</strong>';
+        $a2 = '<strong>holaaaaa</strong>';
         $foo = uniqid();
 
         return $this->render('@App/default/index.html.twig', array(
             'var1' => $foo,
-            'a' => $a
+            'a' => $a2
         ));
     }
 
