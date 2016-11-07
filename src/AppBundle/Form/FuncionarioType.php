2eb53e16dafebf507493fd6148a62d35929eec35
Juan Ardissone <jardissone@juan.ardissone>
2016-11-07 17:37:20 -0300

    Se agregó el formulario

---
+++ src/AppBundle/Form/FuncionarioType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FuncionarioType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellido')
+            ->add('documentoIdentidad')
+            ->add('tipoDocumento', ChoiceType::class, array(
+                'choices' => array_flip($options['tiposDocumentosOptions'])
+            ))
+            ->add('fechaNacimiento')
+            ->add('sexo')
+            ->add('salario')
+            ->add('email')
+            ->add('fechaInicio')
+            ->add('fechaEntrada')
+            ->add('fechaSalida')
+            ->add('curriculum', FileType::class)
+            ->add('observaciones')
+            ->add('dependencia');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired('tiposDocumentosOptions');
+
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Funcionario'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'funcionario';
+    }
+
+
+}
